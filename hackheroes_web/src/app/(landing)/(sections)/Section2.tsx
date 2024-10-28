33afa612b16fadbaf5c2611a6d4ef12ca527d4fc
oskaars <oskora95@uczen.tl.krakow.pl>
2024-10-28 18:03:21 +0100

    test

--- hackheroes_web/src/app/(landing)/(sections)/Section2.tsx
+++ hackheroes_web/src/app/(landing)/(sections)/Section2.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Section2 = () => {
     return (
         <>
-            <section className="p-8 bg-white w-full flex place-content-evenly">
+            <section className="p-[20vh] bg-green-light w-full flex place-content-evenly">
                 <div className="flex items-center p-8 space-x-[10%]">
                     <div className="p-8">
                         <h2 className="text-4xl font-bold text-green-600 bg">Udowodnione naukowo</h2>
--- hackheroes_web/src/app/(landing)/(sections)/Section3.tsx
+++ hackheroes_web/src/app/(landing)/(sections)/Section3.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Section3 = () => {
+const Section2 = () => {
     return (
         <>
-            <section className="p-8 bg-white w-full flex place-content-evenly">
+            <section className="p-[20vh] bg-white w-full flex place-content-evenly">
                 <div className="flex items-center p-8 space-x-[10%]">
+
                     <img src="/duolingo-scientific.png" alt="Ikonki" className="w-[50vh]"/>
-                    <div className="w-1/2 ">
-                        <h2 className="text-4xl font-bold text-green-600 ">Udowodnione naukowo</h2>
+                    <div className="p-8">
+                        <h2 className="text-4xl font-bold text-green-600 bg">Udowodnione naukowo</h2>
                         <p>Łączymy udowodnione naukowo metody nauczania oraz ciekawe, zabawne treści...</p>
                     </div>
                 </div>
@@ -18,4 +19,4 @@ const Section3 = () => {
 };
 
 
-export default Section3;
+export default Section2;
