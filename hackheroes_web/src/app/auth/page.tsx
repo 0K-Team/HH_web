c860ad7f227f2dea576f52a4114c1ec51ec69e73
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2024-10-31 15:59:50 +0100

    login page

--- hackheroes_web/src/app/auth/page.tsx
+++ hackheroes_web/src/app/auth/page.tsx
@@ -1,23 +1,39 @@
 import Link from "next/link";
 import Image from 'next/image';
-import logoLightTransparent from './logoLightTransparent.png';
+import logoTransparent from '../auth/(content)/logoTransparent.svg';
 
 const Login = () => {
     return (
         <>
-            <section className="flex justify-center items-center bg-white-200 h-[100vh]">
-                <div className="text-center bg-white-200 px-[15vh] py-[15vh] rounded-2xl ">
-                   <div className="flex justify-center items-center">
-                        <Image className="w-[15rem] h-[15rem] object-cover"
-                            src={logoLightTransparent}
+            <section className="flex justify-center items-center bg-[#1E1E1E] min-h-screen">
+                <div className="absolute w-[55vh] h-[55vh] bg-[#29FF77] opacity-10 rounded-full top-[-5vh] right-[18%] blur-2xl"></div>
+                <div className="absolute w-[40vh] h-[40vh] bg-[#29FF77] opacity-10 rounded-full bottom-2 left-2 blur-3xl"></div>
+                <div className="absolute w-[55vh] h-[55vh] bg-[#29FF77] opacity-10 rounded-full bottom-[-5vh] right-2 blur-3xl"></div>
+
+                <div
+                    className="text-center bg-[#2A2A2A] px-12 py-16 rounded-2xl border-4 border-[#29FF77] shadow-lg w-full max-w-md">
+                    <div className="flex justify-center items-center mb-3">
+                        <Image
+                            className="w-[30vh] h-[25vh] object-cover"
+                            src={logoTransparent}
                             alt="Logo"
-                            width={200}
-                            height={200}
+                            width={192}
+                            height={192}
                         />
                     </div>
-                    <Link href="/hackheroes_web/src/app/auth/(login)/google-login" className="block text-lg font-bold bg-green-400 p-4 rounded shadow-lg mb-7 hover:bg-green-200 hover:scale-105 transition-transform duration-300">Login with Google</Link>
-                    <Link href="/hackheroes_web/src/app/auth/(login)/meta-login" className="block text-lg font-bold bg-green-400 p-4 rounded shadow-lg hover:bg-green-200 hover:scale-105 transition-transform duration-300">Login with Meta (Facebook)</Link>                </div>
+                    <h2 className="text-white font-semibold text-2xl mb-8">
+                        Zaloguj się używając <br/> jednego ze sposobów:
+                    </h2>
+                    <Link href="/auth/google-login" className="block text-lg font-semibold text-center text-black bg-[#29FF77] py-3 rounded-full mb-6 transition duration-300 ease-in-out transform hover:bg-[#22cc66] hover:scale-105 hover:text-white shadow-lg">
+                        Google
+                    </Link>
+
+                    <Link href="/auth/meta-login" className="block text-lg font-semibold text-center text-black bg-[#29FF77] py-3 rounded-full transition duration-300 ease-in-out transform hover:bg-[#22cc66] hover:scale-105 hover:text-white shadow-lg">
+                        Meta (Facebook)
+                    </Link>
+                </div>
             </section>
+
         </>
     );
 };
