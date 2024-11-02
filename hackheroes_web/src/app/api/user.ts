901c7d1a30c0a938099b0e4845d3b11bde373015
GGodPL <46885632+GGodPL@users.noreply.github.com>
2024-11-02 12:20:37 +0100

    api changes

--- hackheroes_web/src/app/api/user.ts
+++ hackheroes_web/src/app/api/user.ts
@@ -6,19 +6,19 @@ export const fetchLoggedUser = async () :Promise <User> =>{
     return await response.json() as User;
 }
 export const changeUsername = async (name: string) :Promise <User> =>{
-    const response = await fetch(`${baseUrl}user/me/username/${name}`, {method: 'PATCH'});
+    const response = await fetch(`${baseUrl}user/me/username`, {method: 'PATCH', body: JSON.stringify({ username: name })});
     return await response.json() as User;
 }
 
 export const changeFirstName = async (name: string) :Promise <User> =>{
-    const response = await fetch(`${baseUrl}user/me/firstName/${name}`, {method: 'PATCH'});
+    const response = await fetch(`${baseUrl}user/me/firstName`, {method: 'PATCH', body: JSON.stringify({ firstName: name })});
     return await response.json() as User;
 }
 export const changeLastName = async (name: string) :Promise <User> =>{
-    const response = await fetch(`${baseUrl}user/me/lastName/${name}`, {method: 'PATCH'});
+    const response = await fetch(`${baseUrl}user/me/lastName`, {method: 'PATCH', body: JSON.stringify({ lastName: name })});
     return await response.json() as User;
 }
 export const changeLocation = async (name: string) :Promise <User> =>{
-    const response = await fetch(`${baseUrl}user/me/location/${name}`, {method: 'PATCH'});
+    const response = await fetch(`${baseUrl}user/me/location`, {method: 'PATCH', body: JSON.stringify({ location: name })});
     return await response.json() as User;
 }
