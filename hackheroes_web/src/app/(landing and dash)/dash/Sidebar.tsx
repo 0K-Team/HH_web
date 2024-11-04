65ff56526e5e8aa1a5cb5af7e7f6bd49baf4c290
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2024-11-04 16:27:40 +0100

    sidebar fix

--- hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
+++ hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faSeedling, faUser } from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
+import "../globals.css";
 
 export default function Sidebar() {
     const handleMouseEnter = () => {
