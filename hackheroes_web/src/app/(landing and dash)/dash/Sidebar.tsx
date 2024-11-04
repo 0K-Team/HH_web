f80ff495f2f3ee02e5219deb4746bb4ae015c06f
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2024-11-04 16:46:53 +0100

    sidebar fixes

--- hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
+++ hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faSeedling, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faSeedling } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import Link from "next/link";
-import "../globals.css"; // Importing the globals.css file
 
-export default function Sidebar() {
+export default function Home() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const handleMouseEnter = () => {
@@ -17,27 +18,42 @@ export default function Sidebar() {
     };
 
     return (
-        <div className="flex h-screen">
-            <div
-                className={`fixed top-0 left-0 flex flex-col justify-between p-4 space-y-6 bg-black h-screen transition-width duration-300 ${isSidebarOpen ? 'w-[200px]' : 'w-[80px]'}`}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            >
+        <div className="flex">
+            <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`fixed left-0 top-0 h-full bg-gray-dark text-white transition-transform duration-300 max-w-xs w-full ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <nav className="flex flex-col p-4 pt-[8vh]">
-                    <Link href="../dash" className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700 transition-all">
-                        <FontAwesomeIcon icon={faHouse} className="text-2xl text-white" />
-                        {isSidebarOpen && <span className="text-white">Panel użytkownika</span>}
+                    <Link href="../../garden" className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2 text-center">
+                        <FontAwesomeIcon icon={faSeedling} style={{color: "#29ff77",}} />
+                        Wirtualny ogródek
                     </Link>
-                    <Link href="../../garden" className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700 transition-all">
-                        <FontAwesomeIcon icon={faSeedling} className="text-2xl text-white" />
-                        {isSidebarOpen && <span className="text-white">Wirtualny ogródek</span>}
+                    <Link href="../posts" className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2 text-center">
+                        <FontAwesomeIcon icon={faUser} style={{color: "#29ff77",}} />
+                        Sociale
                     </Link>
-                    <Link href="../posts" className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700 transition-all">
-                        <FontAwesomeIcon icon={faUser} className="text-2xl text-white" />
-                        {isSidebarOpen && <span className="text-white">Sociale</span>}
-                    </Link>
+                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
+                        EcoCalculator
+                    </button>
+                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
+                        EcoMaps
+                    </button>
+                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
+                        akcja 4
+                    </button>
+                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
+                        akcja 4
+                    </button>
+                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
+                        akcja 4
+                    </button>
+                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
+                        akcja 4
+                    </button>
                 </nav>
             </div>
+
+            <Link href="../dash" className="absolute top-4 left-[4vh] font-bold bg-gray-light text-green-green py-2 px-4 rounded-lg hover:shadow-2xl transition-all" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <FontAwesomeIcon className="pr-2" icon={faHouse} style={{color: "#29ff77",}} />
+                Panel użytkownika
+            </Link>
         </div>
     );
 }
