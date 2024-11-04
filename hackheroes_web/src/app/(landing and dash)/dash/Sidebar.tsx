77329d05b8ae2ecdc1a69ad27b1a75b3b84ec8f2
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2024-11-04 16:24:38 +0100

    sidebar change

--- hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
+++ hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
@@ -1,47 +1,32 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
-import { faSeedling } from '@fortawesome/free-solid-svg-icons'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faSeedling, faUser } from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
 
-export default function Home() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
+export default function Sidebar() {
     const handleMouseEnter = () => {
-        setIsSidebarOpen(true);
     };
 
     const handleMouseLeave = () => {
-        setIsSidebarOpen(false);
     };
 
     return (
         <div className="flex">
-            <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`fixed left-0 top-0 h-full bg-gray-dark text-white transition-transform duration-300 max-w-xs w-full ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                <nav className="flex flex-col p-4 pt-[8vh]">
-                    <Link href="../../garden" className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2 text-center">
-                        <FontAwesomeIcon className="pr-2" icon={faSeedling} style={{color: "#29ff77",}} />
-                        Wirtualny ogródek
-                    </Link>
-                    <Link href="../posts" className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2 text-center">
-                        <FontAwesomeIcon className="pr-2" icon={faUser} style={{color: "#29ff77",}} />
-                        Sociale
-                    </Link>
-                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
-                        EcoCalculator
-                    </button>
-                    <button className="font-bold bg-gray-light text-green-green p-5 rounded-lg hover:shadow-2xl transition-all mb-2">
-                        EcoMaps
-                    </button>
-                </nav>
+            <div className="slim-sidebar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <Link href="../dash" className="icon">
+                    <FontAwesomeIcon icon={faHouse} style={{color: "#29ff77",}} />
+                    <span className="text">Panel użytkownika</span>
+                </Link>
+                <Link href="../../garden" className="icon">
+                    <FontAwesomeIcon icon={faSeedling} style={{color: "#29ff77",}} />
+                    <span className="text">Wirtualny ogródek</span>
+                </Link>
+                <Link href="../posts" className="icon">
+                    <FontAwesomeIcon icon={faUser} style={{color: "#29ff77",}} />
+                    <span className="text">Sociale</span>
+                </Link>
             </div>
-
-            <Link href="../dash" className="absolute top-4 left-[4vh] font-bold bg-gray-light text-green-green py-2 px-4 rounded-lg hover:shadow-2xl transition-all" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <FontAwesomeIcon className="pr-2" icon={faHouse} style={{color: "#29ff77",}} />
-                Panel użytkownika
-            </Link>
         </div>
     );
 }
