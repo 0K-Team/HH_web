9fec4848934a5e1f4f464aec67f588dd3f99058e
Mateusz <mmakaryk99@uczen.tl.krakow.pl>
2024-11-04 17:17:59 +0100

    sidebar transition fix

--- hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
+++ hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
@@ -16,49 +16,55 @@ export default function Sidebar() {
     };
 
     return (
-        <div
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className={`fixed top-0 left-0 h-full bg-gray-dark text-white transition-all duration-500 ${isSidebarOpen ? "w-64" : "w-20"} flex flex-col items-start p-4 z-50 shadow-lg`}
-        >
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`fixed top-0 left-0 h-full bg-gray-dark text-white transition-all duration-500 ${isSidebarOpen ? "w-64" : "w-20"} flex flex-col items-start p-4 z-50 shadow-lg`}>
             <nav className="flex flex-col space-y-4 mt-10">
                 <Link href="../dash">
-                    <button className={`flex items-center p-3 rounded-lg bg-gray-light transition-all duration-300 ${isSidebarOpen ? "opacity-100 w-full" : "opacity-0 w-0"} text-green-green hover:bg-gray-light hover:shadow-lg`}>
+                    <button
+                        className={`flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "p-3 bg-gray-light w-full" : "p-0 w-10 justify-center"} text-green-green hover:bg-gray-light hover:shadow-lg`}
+                    >
                         <FontAwesomeIcon icon={faHouse} className="text-2xl text-green-green" />
-                        <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"}`}>
+                        <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
                             Panel użytkownika
                         </span>
                     </button>
                 </Link>
 
                 <Link href="../../garden">
-                    <button className={`flex items-center p-3 rounded-lg bg-gray-light transition-all duration-300 ${isSidebarOpen ? "opacity-100 w-full" : "opacity-0 w-0"} text-green-green hover:bg-gray-light hover:shadow-lg`}>
+                    <button
+                        className={`flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "p-3 bg-gray-light w-full" : "p-0 w-10 justify-center"} text-green-green hover:bg-gray-light hover:shadow-lg`}
+                    >
                         <FontAwesomeIcon icon={faSeedling} className="text-2xl text-green-green" />
-                        <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"}`}>
+                        <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
                             Wirtualny ogródek
                         </span>
                     </button>
                 </Link>
 
                 <Link href="../posts">
-                    <button className={`flex items-center p-3 rounded-lg bg-gray-light transition-all duration-300 ${isSidebarOpen ? "opacity-100 w-full" : "opacity-0 w-0"} text-green-green hover:bg-gray-light hover:shadow-lg`}>
+                    <button
+                        className={`flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "p-3 bg-gray-light w-full" : "p-0 w-10 justify-center"} text-green-green hover:bg-gray-light hover:shadow-lg`}
+                    >
                         <FontAwesomeIcon icon={faUser} className="text-2xl text-green-green" />
-                        <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"}`}>
+                        <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
                             Sociale
                         </span>
                     </button>
                 </Link>
 
-                <button className={`flex items-center p-3 rounded-lg bg-gray-light transition-all duration-300 ${isSidebarOpen ? "opacity-100 w-full" : "opacity-0 w-0"} text-green-green hover:bg-gray-light hover:shadow-lg`}>
+                <button
+                    className={`flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "p-3 bg-gray-light w-full" : "p-0 w-10 justify-center"} text-green-green hover:bg-gray-light hover:shadow-lg`}
+                >
                     <FontAwesomeIcon icon={faCalculator} className="text-2xl text-green-green" />
-                    <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"}`}>
+                    <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
                         EcoCalculator
                     </span>
                 </button>
 
-                <button className={`flex items-center p-3 rounded-lg bg-gray-light transition-all duration-300 ${isSidebarOpen ? "opacity-100 w-full" : "opacity-0 w-0"} text-green-green hover:bg-gray-light hover:shadow-lg`}>
+                <button
+                    className={`flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "p-3 bg-gray-light w-full" : "p-0 w-10 justify-center"} text-green-green hover:bg-gray-light hover:shadow-lg`}
+                >
                     <FontAwesomeIcon icon={faMap} className="text-2xl text-green-green" />
-                    <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"}`}>
+                    <span className={`ml-4 font-bold transition-opacity duration-500 delay-200 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
                         EcoMaps
                     </span>
                 </button>
