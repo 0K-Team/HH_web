5b2b70d8e411d9f8306a85f38ff7fb9355bed368
oskaars <oskora95@uczen.tl.krakow.pl>
2024-11-05 23:00:53 +0100

    sidebar add

--- hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
+++ hackheroes_web/src/app/(landing and dash)/dash/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faSeedling, faUser, faCalculator, faMap } from "@fortawesome/free-solid-svg-icons";
+import {faHouse, faSeedling, faUser, faCalculator, faMap, faQuestion} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { logout } from "@/app/api/user";
 
@@ -42,6 +42,15 @@ export default function Sidebar() {
                         <FontAwesomeIcon icon={faUser} className="absolute left-4 text-2xl text-green-green"/>
                         <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
                            Sociale
+                        </span>
+                    </button>
+                </Link>
+
+                <Link href="../../quiz">
+                    <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
+                        <FontAwesomeIcon icon={faQuestion} className="absolute left-4 text-2xl text-green-green"/>
+                        <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
+                           Quiz
                         </span>
                     </button>
                 </Link>
