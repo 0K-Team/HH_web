6a3122173c526472f83ce9e2c6ba4aace9cbafae
oskaars <oskora95@uczen.tl.krakow.pl>
2024-11-05 19:08:22 +0100

    garden styling

--- hackheroes_web/src/app/garden/page.tsx
+++ hackheroes_web/src/app/garden/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Garden from './(components)/(Garden)/Garden';
 import { UserProvider } from '@/app/garden/(context)/UserContext';
-
+import Sidebar from "@/app/(landing and dash)/dash/Sidebar";
 const GardenPage: React.FC = () => {
     return (
         <UserProvider>
+            <Sidebar/>
             <div className="container mx-auto p-4">
                 <h1 className="text-2xl font-bold mb-4 text-center">Mój Wirtualny Ogródek</h1>
                 <Garden />
