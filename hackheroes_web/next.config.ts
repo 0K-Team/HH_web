78e996154e55f2471e6f725196a89e9c3d005aa0
GGodPL <46885632+GGodPL@users.noreply.github.com>
2024-11-10 19:52:10 +0100

    hotfix

--- hackheroes_web/next.config.ts
+++ hackheroes_web/next.config.ts
@@ -1,22 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ecohero.q1000q.me',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    minimumCacheTTL: 60
-  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3000/:path*'
+        destination: 'http://192.168.0.10:8812/:path*'
+
       },
     ]
   }
