dc78b6d7ea66f165b147e12b77a38696c8e1174d
Karol Gajda <jestemdobrywniczym@gmail.com>
2024-11-11 20:07:30 +0100

    Correct landing page migration problems

--- hackheroes_web/src/app/dash/Sidebar.tsx
+++ hackheroes_web/src/app/dash/Sidebar.tsx
@@ -19,7 +19,7 @@ export default function Sidebar() {
     return (
         <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`fixed top-0 left-0 h-full bg-gray-light rounded-r-xl text-white transition-all duration-500 ${isSidebarOpen ? "w-86" : "w-20"} flex flex-col items-start p-4 z-50 shadow-lg`}>
             <nav className="flex flex-col space-y-4 mt-10">
-                <Link href="../(landing%20and%20dash)/dash">
+                <Link href="../dash">
                     <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
                         <FontAwesomeIcon icon={faHouse} className="absolute left-4 text-2xl text-green-green"/>
                         <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
@@ -37,7 +37,7 @@ export default function Sidebar() {
                     </button>
                 </Link>
 
-                <Link href="../(landing%20and%20dash)/posts">
+                <Link href="../posts">
                     <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
                         <FontAwesomeIcon icon={faUser} className="absolute left-4 text-2xl text-green-green"/>
                         <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
