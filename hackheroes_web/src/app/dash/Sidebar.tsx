56c87207ff5fa2002df11843610286a482a9ba8c
Q1000Q <1000q.mj@gmail.com>
2024-11-12 18:56:39 +0100

    Made sidebar somewhat less shitty

--- hackheroes_web/src/app/dash/Sidebar.tsx
+++ hackheroes_web/src/app/dash/Sidebar.tsx
@@ -1,9 +1,27 @@
 "use client";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faSeedling, faUser, faCalculator, faMap, faQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faSeedling, faUser, faCalculator, faMap, faQuestion, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { logout } from "@/app/api/user";
+
+interface SidebarButtonProps {
+    href: string;
+    icon: import("@fortawesome/fontawesome-svg-core").IconDefinition;
+    label: string;
+    isSidebarOpen: boolean;
+}
+
+const SidebarButton: React.FC<SidebarButtonProps> = ({ href, icon, label, isSidebarOpen }) => (
+    <Link href={href}>
+        <button className={`relative flex items-center rounded-lg transition-all duration-300 mt-2 h-14 ${isSidebarOpen ? "pl-10 pr-4 bg-gray-dark w-full" : "justify-center"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
+            <FontAwesomeIcon icon={icon} className="absolute left-4 text-2xl text-green-green" />
+            <span className={`ml-12 font-bold transition-opacity w-48 flex duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
+                {label}
+            </span>
+        </button>
+    </Link>
+);
 
 export default function Sidebar() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -17,64 +35,16 @@ export default function Sidebar() {
     };
 
     return (
-        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`fixed top-0 left-0 h-full bg-gray-light rounded-r-xl text-white transition-all duration-500 ${isSidebarOpen ? "w-86" : "w-20"} flex flex-col items-start p-4 z-50 shadow-lg`}>
-            <nav className="flex flex-col space-y-4 mt-10">
-                <Link href="../dash">
-                    <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                        <FontAwesomeIcon icon={faHouse} className="absolute left-4 text-2xl text-green-green"/>
-                        <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                           Panel użytkownika
-                        </span>
-                    </button>
-                </Link>
-
-                <Link href="../garden">
-                    <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                        <FontAwesomeIcon icon={faSeedling} className="absolute left-4 text-2xl text-green-green"/>
-                        <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                           Wirtualny ogródek
-                        </span>
-                    </button>
-                </Link>
-
-                <Link href="../posts">
-                    <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                        <FontAwesomeIcon icon={faUser} className="absolute left-4 text-2xl text-green-green"/>
-                        <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                           Sociale
-                        </span>
-                    </button>
-                </Link>
-
-                <Link href="../quiz">
-                    <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                        <FontAwesomeIcon icon={faQuestion} className="absolute left-4 text-2xl text-green-green"/>
-                        <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                           Quiz
-                        </span>
-                    </button>
-                </Link>
-
-                <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                    <FontAwesomeIcon icon={faCalculator} className="absolute left-4 text-2xl text-green-green" />
-                    <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                        EcoCalculator
-                    </span>
-                </button>
-
-                <button className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                    <FontAwesomeIcon icon={faMap} className="absolute left-4 text-2xl text-green-green" />
-                    <span className={`ml-12 font-bold transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                        EcoMaps
-                    </span>
-                </button>
-
-                <button onClick={logout} className={`relative flex items-center rounded-lg transition-all duration-300 ${isSidebarOpen ? "pl-10 pr-4 py-3 bg-gray-dark w-full" : "justify-center w-10 h-10"} text-green-green hover:bg-gray-lighter hover:shadow-lg`}>
-                    <span className={`ml-12 font-bold transition-opacity duration-300  ${isSidebarOpen ? "opacity-100" : "opacity-0"} whitespace-nowrap`}>
-                        Logout
-                    </span>
-                </button>
-            </nav>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`fixed top-0 left-0 h-full bg-gray-light rounded-r-xl text-white transition-all duration-300 ${isSidebarOpen ? "w-86" : "w-20"} flex-col items-start p-4 z-50 shadow-lg`}>
+            <SidebarButton href="../dash" icon={faHouse} label="Panel użytkownika" isSidebarOpen={isSidebarOpen} />
+            <SidebarButton href="../garden" icon={faSeedling} label="Wirtualny ogródek" isSidebarOpen={isSidebarOpen} />
+            <SidebarButton href="../posts" icon={faUser} label="Sociale" isSidebarOpen={isSidebarOpen} />
+            <SidebarButton href="../quiz" icon={faQuestion} label="Quiz" isSidebarOpen={isSidebarOpen} />
+            <SidebarButton href="#" icon={faCalculator} label="EcoCalculator" isSidebarOpen={isSidebarOpen} />
+            <SidebarButton href="#" icon={faMap} label="EcoMaps" isSidebarOpen={isSidebarOpen} />
+            <a onClick={logout}>
+                <SidebarButton href="#" icon={faSignOutAlt} label="Logout" isSidebarOpen={isSidebarOpen} />
+            </a>
         </div>
     );
 }
