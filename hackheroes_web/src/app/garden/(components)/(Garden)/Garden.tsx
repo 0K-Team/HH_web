2568e9d7f42a4a38cc1088f91c4fe31528071670
oskaars <oskora95@uczen.tl.krakow.pl>
2024-11-15 18:26:25 +0100

    style

--- hackheroes_web/src/app/garden/(components)/(Garden)/Garden.tsx
+++ hackheroes_web/src/app/garden/(components)/(Garden)/Garden.tsx
@@ -34,14 +34,14 @@ const Garden: React.FC = () => {
             <div className="p-6 grid grid-cols-3 gap-4">
                 {gardenData.plants.map((plant: Plant) => (
 
-                    <div key={plant._id} className="drop-shadow-lg text-green-green rounded bg-gray-dark pt-8 w-full m-0 flex flex-col items-center jusitfy-center h-full">
+                    <div key={plant._id} className=" mb-4 drop-shadow-lg text-green-green rounded bg-gray-dark pt-8 w-full m-0 flex flex-col items-center jusitfy-center h-full">
                         <Image
                             className="drop-shadow-lg rounded-lg"
                             src={`/api/v1/CDN/${plant.type}`}
                             alt={plant.name}
                         />
                         <h2 className="font-bold">{plant.name}</h2>
-                        <p className="font-bold mb-4">Growth Stage: {plant.growthStage}</p>
+                        <p className="font-bold ">Growth Stage: {plant.growthStage}</p>
                     </div>
                 ))}
             </div>
