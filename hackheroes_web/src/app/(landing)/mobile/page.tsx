9ac8e54f8f3a722d9f4db58c2b8b18a5a1560317
Karol Gajda <jestemdobrywniczym@gmail.com>
2024-11-15 13:26:30 +0100

    Update mobile description

--- hackheroes_web/src/app/(landing)/mobile/page.tsx
+++ hackheroes_web/src/app/(landing)/mobile/page.tsx
@@ -177,7 +177,8 @@ export default function Home() {
                             "https://i.imgur.com/8aI0xFU.png",
                             "https://i.imgur.com/FCfLDoz.png",
                             "https://i.imgur.com/6X15dGv.png",
-                            "https://i.imgur.com/NIgLi6s.png"
+                            "https://i.imgur.com/NIgLi6s.png",
+                            "https://i.imgur.com/qkqWE1c.png"
                         ].map((src, index) => (
                             <div key={index} className="w-full md:w-1/3 p-2">
                                 <Image
